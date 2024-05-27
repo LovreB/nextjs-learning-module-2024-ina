b84df380cd8c1b424d7ea7263b39dcae28c79f84
Katarina Bergbom <kbergbom@volvocars.com>
2024-05-27 18:02:20 +0200

    Test ENV

--- utils/getToken.ts
+++ utils/getToken.ts
@@ -4,6 +4,8 @@ export const getToken = async () => {
   // msalInstance.loginRedirect({
   //   scopes: ["api://717aaf54-6553-432e-85ca-b2399c7f87f6/.default"],
   // });
+  console.log("inGetToken");
+  console.log({ username: process.env.NEXT_PUBLIC_USERNAME });
   await msalInstance.ssoSilent({
     scopes: ["api://717aaf54-6553-432e-85ca-b2399c7f87f6/.default"],
     loginHint: process.env.NEXT_PUBLIC_USERNAME,
